39b7c5ad5e4fab5199520026a6589d7c3ff879f7
donhuole <donhuole@gmail.com>
2024-01-22 08:27:31 +0000

    modify proto

--- fabric-plugin/src/main/java/com/mit/GrpcServer.java
+++ fabric-plugin/src/main/java/com/mit/GrpcServer.java
@@ -1,8 +1,8 @@
 package com.mit;
 
-import com.mit.HelloGrpc;
-import com.mit.HelloOuterClass.HelloRequest;
-import com.mit.HelloOuterClass.HelloResponse;
+// import com.mit.HelloGrpc;
+// import com.mit.HelloOuterClass.HelloRequest;
+// import com.mit.HelloOuterClass.HelloResponse;
 import io.grpc.stub.StreamObserver;
 import io.grpc.BindableService;
 import io.grpc.Grpc;
@@ -14,22 +14,22 @@ import java.util.concurrent.TimeUnit;
 public class GrpcServer {
     private Server server;
     private void start() throws IOException {
-        int port = 44444;
-        server = ServerBuilder.forPort(port)
-                .addService((BindableService) new HelloImpl())
-                .build()
-                .start();
-        System.out.println("Server started, listening on " + port);
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            // jvm关闭前执行
-            System.err.println("*** shutting down gRPC server since JVM is shutting down");
-            try {
-                GrpcServer.this.stop();
-            } catch (InterruptedException e) {
-                e.printStackTrace(System.err);
-            }
-            System.err.println("*** server shut down");
-        }));
+        // int port = 44444;
+        // server = ServerBuilder.forPort(port)
+        //         .addService((BindableService) new HelloImpl())
+        //         .build()
+        //         .start();
+        // System.out.println("Server started, listening on " + port);
+        // Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+        //     // jvm关闭前执行
+        //     System.err.println("*** shutting down gRPC server since JVM is shutting down");
+        //     try {
+        //         GrpcServer.this.stop();
+        //     } catch (InterruptedException e) {
+        //         e.printStackTrace(System.err);
+        //     }
+        //     System.err.println("*** server shut down");
+        // }));
     }
 
     private void stop() throws InterruptedException {
@@ -54,14 +54,14 @@ public class GrpcServer {
         server.blockUntilShutdown();
     }
 
-    private class HelloImpl extends HelloGrpc.HelloImplBase{
+    // private class HelloImpl extends HelloGrpc.HelloImplBase{
 
-        @Override
-        public void sayHello(HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
-            HelloResponse helloResponse = HelloResponse.newBuilder().setMessage("Hello "+request.getName()+", I'm Java grpc Server").build();
-            responseObserver.onNext(helloResponse);
-            responseObserver.onCompleted();
-        }
-    }
+    //     @Override
+    //     public void sayHello(HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
+    //         HelloResponse helloResponse = HelloResponse.newBuilder().setMessage("Hello "+request.getName()+", I'm Java grpc Server").build();
+    //         responseObserver.onNext(helloResponse);
+    //         responseObserver.onCompleted();
+    //     }
+    // }
     
 }
--- fabric-plugin/src/main/java/com/mit/GrpcService.java
+++ fabric-plugin/src/main/java/com/mit/GrpcService.java
@@ -6,15 +6,15 @@ import io.grpc.ServerMethodDefinition;
 public class GrpcService {
 
   public static void main(String[] args) throws Exception {
-    // 创建 gRPC 服务器
-    Server server = ServerBuilder.forPort(55555)
-        .addService()
-        .build();
+  //   // 创建 gRPC 服务器
+  //   Server server = ServerBuilder.forPort(55555)
+  //       .addService(null)
+  //       .build();
 
-    // 启动服务器
-    server.start();
+  //   // 启动服务器
+  //   server.start();
 
-    // 保持服务器运行
-    server.awaitTermination();
+  //   // 保持服务器运行
+  //   server.awaitTermination();
   }
 }
